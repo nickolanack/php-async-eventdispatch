d532854beca5a0de00a5de00565a1292aa5d7282
Nicholas  Blackwell <nickblackwell82@gmail.com>
2020-10-20 20:47:15 -0700

    check for autoload in as subproject

--- src/schedule.php
+++ src/schedule.php
@@ -1,7 +1,11 @@
 <?php
 
+if(file_exists(__DIR__ . '/../vendor/autoload.php')){
+	include_once __DIR__ . '/../vendor/autoload.php';
+}else{
+	include_once __DIR__ . '/../../../autoload.php';
+}
 
-include_once __DIR__ . '/../vendor/autoload.php';
 
 $longOpts = array(
 	'schedule:',
